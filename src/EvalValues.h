6c29a11917ef1dc97ba316410221bfb0923159a5
M3rg1 <m3rg17@gmail.com>
2021-09-18 18:37:42 +0200

    constexpr

--- src/EvalValues.h
+++ src/EvalValues.h
@@ -5,12 +5,12 @@
 
 namespace Meetra::EvalValues {
 
-    inline Score mg_value[PIECE_TYPE_NR] = {0, 82, 337, 365, 477, 1025, 0};
-    inline Score eg_value[PIECE_TYPE_NR] = {0, 94, 281, 297, 512, 936, 0};
+    constexpr Score mg_value[PIECE_TYPE_NR] = {0, 82, 337, 365, 477, 1025, 0};
+    constexpr Score eg_value[PIECE_TYPE_NR] = {0, 94, 281, 297, 512, 936, 0};
 
 #define FLIP(sq) ((sq)^56)
 
-    inline Score mg_pawn_table[SQUARE_NR] = {
+    constexpr Score mg_pawn_table[SQUARE_NR] = {
             0, 0, 0, 0, 0, 0, 0, 0,
             98, 134, 61, 95, 68, 126, 34, -11,
             -6, 7, 26, 31, 65, 56, 25, -20,
@@ -21,7 +21,7 @@ namespace Meetra::EvalValues {
             0, 0, 0, 0, 0, 0, 0, 0,
     };
 
-    inline Score eg_pawn_table[SQUARE_NR] = {
+    constexpr Score eg_pawn_table[SQUARE_NR] = {
             0, 0, 0, 0, 0, 0, 0, 0,
             178, 173, 158, 134, 147, 132, 165, 187,
             94, 100, 85, 67, 56, 53, 82, 84,
@@ -32,7 +32,7 @@ namespace Meetra::EvalValues {
             0, 0, 0, 0, 0, 0, 0, 0,
     };
 
-    inline Score mg_knight_table[SQUARE_NR] = {
+    constexpr Score mg_knight_table[SQUARE_NR] = {
             -167, -89, -34, -49, 61, -97, -15, -107,
             -73, -41, 72, 36, 23, 62, 7, -17,
             -47, 60, 37, 65, 84, 129, 73, 44,
@@ -43,7 +43,7 @@ namespace Meetra::EvalValues {
             -105, -21, -58, -33, -17, -28, -19, -23,
     };
 
-    inline Score eg_knight_table[SQUARE_NR] = {
+    constexpr Score eg_knight_table[SQUARE_NR] = {
             -58, -38, -13, -28, -31, -27, -63, -99,
             -25, -8, -25, -2, -9, -25, -24, -52,
             -24, -20, 10, 9, -1, -9, -19, -41,
@@ -54,7 +54,7 @@ namespace Meetra::EvalValues {
             -29, -51, -23, -15, -22, -18, -50, -64,
     };
 
-    inline Score mg_bishop_table[SQUARE_NR] = {
+    constexpr Score mg_bishop_table[SQUARE_NR] = {
             -29, 4, -82, -37, -25, -42, 7, -8,
             -26, 16, -18, -13, 30, 59, 18, -47,
             -16, 37, 43, 40, 35, 50, 37, -2,
@@ -65,7 +65,7 @@ namespace Meetra::EvalValues {
             -33, -3, -14, -21, -13, -12, -39, -21,
     };
 
-    inline Score eg_bishop_table[SQUARE_NR] = {
+    constexpr Score eg_bishop_table[SQUARE_NR] = {
             -14, -21, -11, -8, -7, -9, -17, -24,
             -8, -4, 7, -12, -3, -13, -4, -14,
             2, -8, 0, -1, -2, 6, 0, 4,
@@ -76,7 +76,7 @@ namespace Meetra::EvalValues {
             -23, -9, -23, -5, -9, -16, -5, -17,
     };
 
-    inline Score mg_rook_table[SQUARE_NR] = {
+    constexpr Score mg_rook_table[SQUARE_NR] = {
             32, 42, 32, 51, 63, 9, 31, 43,
             27, 32, 58, 62, 80, 67, 26, 44,
             -5, 19, 26, 36, 17, 45, 61, 16,
@@ -87,7 +87,7 @@ namespace Meetra::EvalValues {
             -19, -13, 1, 17, 16, 7, -37, -26,
     };
 
-    inline Score eg_rook_table[SQUARE_NR] = {
+    constexpr Score eg_rook_table[SQUARE_NR] = {
             13, 10, 18, 15, 12, 12, 8, 5,
             11, 13, 13, 11, -3, 3, 8, 3,
             7, 7, 7, 5, 4, -3, -5, -3,
@@ -98,7 +98,7 @@ namespace Meetra::EvalValues {
             -9, 2, 3, -1, -5, -13, 4, -20,
     };
 
-    inline Score mg_queen_table[SQUARE_NR] = {
+    constexpr Score mg_queen_table[SQUARE_NR] = {
             -28, 0, 29, 12, 59, 44, 43, 45,
             -24, -39, -5, 1, -16, 57, 28, 54,
             -13, -17, 7, 8, 29, 56, 47, 57,
@@ -109,7 +109,7 @@ namespace Meetra::EvalValues {
             -1, -18, -9, 10, -15, -25, -31, -50,
     };
 
-    inline Score eg_queen_table[SQUARE_NR] = {
+    constexpr Score eg_queen_table[SQUARE_NR] = {
             -9, 22, 22, 27, 27, 19, 10, 20,
             -17, 20, 32, 41, 58, 25, 30, 0,
             -20, 6, 9, 49, 47, 35, 19, 9,
@@ -120,7 +120,7 @@ namespace Meetra::EvalValues {
             -33, -28, -22, -43, -5, -32, -20, -41,
     };
 
-    inline Score mg_king_table[SQUARE_NR] = {
+    constexpr Score mg_king_table[SQUARE_NR] = {
             -65, 23, 16, -15, -56, -34, 2, 13,
             29, -1, -20, -7, -8, -4, -38, -29,
             -9, 24, 2, -16, -20, 6, 22, -22,
@@ -131,7 +131,7 @@ namespace Meetra::EvalValues {
             -15, 36, 12, -54, 8, -28, 24, 14,
     };
 
-    inline Score eg_king_table[SQUARE_NR] = {
+    constexpr Score eg_king_table[SQUARE_NR] = {
             -74, -35, -18, -18, -11, 15, 4, -17,
             -12, 17, 14, 17, 17, 38, 23, 11,
             10, 17, 23, 15, 20, 45, 44, 13,
@@ -142,7 +142,7 @@ namespace Meetra::EvalValues {
             -53, -34, -21, -11, -28, -14, -24, -43
     };
 
-    inline Score no_piece_table[SQUARE_NR] = {
+    constexpr Score no_piece_table[SQUARE_NR] = {
             0, 0, 0, 0, 0, 0, 0, 0,
             0, 0, 0, 0, 0, 0, 0, 0,
             0, 0, 0, 0, 0, 0, 0, 0,
@@ -153,7 +153,7 @@ namespace Meetra::EvalValues {
             0, 0, 0, 0, 0, 0, 0, 0
     };
 
-    inline Score *mg_pesto_table[PIECE_TYPE_NR] =
+    inline const Score *mg_pesto_table[PIECE_TYPE_NR] =
             {
                     no_piece_table,
                     mg_pawn_table,
@@ -164,7 +164,7 @@ namespace Meetra::EvalValues {
                     mg_king_table
             };
 
-    inline Score *eg_pesto_table[PIECE_TYPE_NR] =
+    inline const Score *eg_pesto_table[PIECE_TYPE_NR] =
             {
                     no_piece_table,
                     eg_pawn_table,
@@ -175,7 +175,7 @@ namespace Meetra::EvalValues {
                     eg_king_table
             };
 
-    inline int phase_inc[PIECE_TYPE_NR] = {0, 0, 1, 1, 2, 4, 0};
+    constexpr int phase_inc[PIECE_TYPE_NR] = {0, 0, 1, 1, 2, 4, 0};
     inline Score mg_table[COLOR_NR][PIECE_TYPE_NR][SQUARE_NR];
     inline Score eg_table[COLOR_NR][PIECE_TYPE_NR][SQUARE_NR];
 
