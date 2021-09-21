bb618e65fc37da5c539d5b5b239a7d6f54cfd684
M3rg1 <m3rg17@gmail.com>
2021-09-21 20:17:25 +0200

    ply -> full move but book depth / 2

--- src/Search.cpp
+++ src/Search.cpp
@@ -102,7 +102,7 @@ namespace Meetra::Search {
         InitSearch(s, board);
 
         if (use_book && !settings.fixed_depth && !settings.infinite && !settings.fixed_time && !chess960 &&
-            board.Ply() <= BOOK_DEPTH) {
+            board.FullMoveClock() <= BOOK_DEPTH / 2) {
             auto moves = Book::Probe(board);
             if (!moves.empty()) {
                 StopSearch();
