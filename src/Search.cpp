8f2c941d75eeba81b9ecf357e7b58d7b5136d9aa
M3rg1 <m3rg17@gmail.com>
2021-09-21 20:12:27 +0200

    fullmove clock for book

--- src/Search.cpp
+++ src/Search.cpp
@@ -102,7 +102,7 @@ namespace Meetra::Search {
         InitSearch(s, board);
 
         if (use_book && !settings.fixed_depth && !settings.infinite && !settings.fixed_time && !chess960 &&
-            board.HistorySize() <= BOOK_DEPTH) {
+            board.FullMoveClock() <= BOOK_DEPTH) {
             auto moves = Book::Probe(board);
             if (!moves.empty()) {
                 StopSearch();
