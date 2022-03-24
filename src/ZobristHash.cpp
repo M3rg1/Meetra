ba22b47b428050648db5c68fa48f03ac13233992
M3rg1 <m3rg17@gmail.com>
2022-03-24 23:11:57 +0100

    idk

--- src/ZobristHash.cpp
+++ src/ZobristHash.cpp
@@ -5,79 +5,36 @@
 #include "Bitboards.h"
 #include "Board.h"
 
-namespace Zobrist {
 
-    std::array<std::array<uint64_t, B_KING + 1>, SQUARE_NR> piece_keys;
-    std::array<uint64_t, SQUARE_NR> castling_keys;
-    std::array<uint64_t, SQUARE_NR> ep_keys;
-    uint64_t color_key;
+//    std::array<std::array<uint64_t, B_KING + 1>, SQUARE_NR> piece_keys;
+//    std::array<uint64_t, SQUARE_NR> castling_keys;
+//    std::array<uint64_t, SQUARE_NR> ep_keys;
+//    uint64_t color_key;
 
-    void Init() {
 
-        std::mt19937_64 mt(ZOBRIST_SEED); // NOLINT(cert-msc51-cpp)
-        auto gen = [&] { return mt(); };
 
-        std::ranges::for_each(piece_keys, [&](auto &pt_keys) { std::ranges::generate(pt_keys, gen); });
-        std::ranges::generate(castling_keys, gen);
-        std::ranges::generate(ep_keys, gen);
-        ep_keys[0] = 0;
-        color_key = gen();
-    }
-
-    void AddPiece(Hash64 &h, Piece p, Square s) {
-        h ^= piece_keys[s][p];
-    }
-
-    void RemovePiece(Hash64 &h, Piece p, Square s) {
-        AddPiece(h, p, s);
-    }
-
-    void AddEp(Hash64 &h, Square s) {
-        h ^= ep_keys[s];
-    }
-
-    void RemoveEp(Hash64 &h, Square s) {
-        AddEp(h, s);
-    }
-
-    void UpdateCr(Hash64 &h, Bitboard previous, Bitboard current) {
-        Bitboard cr_change = previous ^ current;
-        while (cr_change) {
-            h ^= castling_keys[Bitboards::PopLsb(cr_change)];
-        }
-    }
-
-    void UpdateColor(Hash64 &h) {
-        h ^= color_key;
-    }
-
-    void MovePiece(Hash64 &h, Piece p, Square from, Square to) {
-        h ^= piece_keys[from][p] ^ piece_keys[to][p];
-    }
-
-    void GenPiecesHash(Hash64 &hash, const Board &board) {
+    void Zobrist::GenPiecesHash(const Board &board) {
         for (Color c: Colors) {
             for (PieceType pt: PieceTypes) {
                 Bitboard pieces = board.Pieces(pt, c);
                 while (pieces) {
                     Square s = Bitboards::PopLsb(pieces);
-                    AddPiece(hash, NewPiece(pt, c), s);
+                    AddPiece(NewPiece(pt, c), s);
                 }
             }
         }
     }
 
-    Hash64 GenHash64(const Board &board) {
+    Hash64 Zobrist::GenHash64(const Board &board) {
 
         Hash64 hash = NEW_HASH64;
 
-        GenPiecesHash(hash, board);
-        UpdateCr(hash, EMPTY_BB, board.Cr());
-        AddEp(hash, board.EpSquare());
+        GenPiecesHash(board);
+        UpdateCr(EMPTY_BB, board.Cr());
+        AddEp(board.EpSquare());
         if (board.ColorToMove() == BLACK) {
-            UpdateColor(hash);
+            UpdateColor();
         }
 
         return hash;
     }
-}
--- src/ZobristHash.h
+++ src/ZobristHash.h
@@ -2,24 +2,82 @@
 #define MEETRA_ZOBRISTHASH_H
 
 #include "Defs.h"
+#include <random>
+#include <algorithm>
+#include <array>
+#include "Bitboards.h"
+#include "Config.h"
 
 class Board;
 
-namespace Zobrist {
 
-    void Init();
+    class Zobrist {
 
-    [[nodiscard]] Hash64 GenHash64(const Board &board);
-    [[nodiscard]] inline Hash16 MakeHash16(Hash64 hash64) { return static_cast<Hash16>(hash64 >> 48); }
+        //inline static auto generator = [] { static std::mt19937_64 mt(ZOBRIST_SEED); return mt(); };
+        inline static std::array<std::array<uint64_t, B_KING + 1>, SQUARE_NR> piece_keys = {};
+        inline static std::array<uint64_t, SQUARE_NR> castling_keys = {};
+        inline static std::array<uint64_t, SQUARE_NR> ep_keys = {};
+        inline static uint64_t color_key = {};
 
-    void AddPiece(Hash64 &h, Piece p, Square s);
-    void RemovePiece(Hash64 &h, Piece p, Square s);
-    void MovePiece(Hash64 &h, Piece p, Square from, Square to);
-    void RemoveEp(Hash64 &h, Square s);
-    void AddEp(Hash64 &h, Square s);
-    void UpdateCr(Hash64 &h, Bitboard previous, Bitboard current);
-    void UpdateColor(Hash64 &h);
+    public:
 
-}
+
+        Hash64 hash_64;
+        Zobrist() = default;
+        void GenPiecesHash(const Board &board);
+
+        Hash64 GenHash64(const Board &board);
+        Zobrist(const Board &board) {
+            GenHash64(board);
+        }
+
+        [[nodiscard]] Hash64 GetHash64() const { return hash_64; }
+        [[nodiscard]] Hash16 GetHash16() const { return static_cast<Hash16>(hash_64 >> 48); }
+
+
+        static void Init() {
+
+            std::mt19937_64 mt(ZOBRIST_SEED); // NOLINT(cert-msc51-cpp)
+            auto generator = [&] { return mt(); };
+
+            std::ranges::for_each(piece_keys, [&](auto &pt_keys) { std::ranges::generate(pt_keys, generator); });
+            std::ranges::generate(castling_keys, generator);
+            std::ranges::generate(ep_keys, generator);
+            ep_keys[0] = 0;
+            color_key = generator();
+        }
+
+        void AddPiece(Piece p, Square s) {
+            hash_64 ^= piece_keys[s][p];
+        }
+
+        void RemovePiece(Piece p, Square s) {
+            AddPiece(p, s);
+        }
+
+        void AddEp(Square s) {
+            hash_64 ^= ep_keys[s];
+        }
+
+        void RemoveEp( Square s) {
+            AddEp(s);
+        }
+
+        void UpdateCr(Bitboard previous, Bitboard current) {
+            Bitboard cr_change = previous ^ current;
+            while (cr_change) {
+                hash_64 ^= castling_keys[Bitboards::PopLsb(cr_change)];
+            }
+        }
+
+        void UpdateColor() {
+            hash_64 ^= color_key;
+        }
+
+        void MovePiece(Piece p, Square from, Square to) {
+            hash_64 ^= piece_keys[from][p] ^ piece_keys[to][p];
+        }
+    };
+
 
 #endif //MEETRA_ZOBRISTHASH_H
